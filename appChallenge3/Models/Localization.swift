7ff241ce47a4fe2079bd42833f1ab716f9d3148c
vicenzorm <vimasera11@gmail.com>
2025-11-18 16:15:02 -0300

    Localization model ready

--- appChallenge3/Models/Localization.swift
+++ appChallenge3/Models/Localization.swift
@@ -4,5 +4,27 @@
 //
 //  Created by Vicenzo Másera on 18/11/25.
 //
-
+import SwiftData
 import Foundation
+
+@Model
+final class Localization {
+    var id: UUID
+    var createdAt: Date
+    
+    var latitude: Double
+    var longitude: Double
+    var altitude: Double
+    
+    var sculpture: Sculpture?
+    
+    var contributors: [Author]?
+    
+    init(latitude: Double, longitude: Double, altitude: Double) {
+        self.id = UUID()
+        self.createdAt = Date()
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+    }
+}
