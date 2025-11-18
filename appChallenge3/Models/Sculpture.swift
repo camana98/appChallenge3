be40d83760d0004dded458d1c8ccc96019c1799f
vicenzorm <vimasera11@gmail.com>
2025-11-18 16:14:15 -0300

    Created new files, deleted the model template and Author model ready

---
+++ appChallenge3/Models/Sculpture.swift
@@ -0,0 +1,8 @@
+//
+//  Sculpture.swift
+//  appChallenge3
+//
+//  Created by Vicenzo Másera on 18/11/25.
+//
+
+import Foundation
---
+++ appChallenge3/Models/UnfinishedCube.swift
@@ -0,0 +1,8 @@
+//
+//  UnfinishedCube.swift
+//  appChallenge3
+//
+//  Created by Vicenzo Másera on 18/11/25.
+//
+
+import Foundation
