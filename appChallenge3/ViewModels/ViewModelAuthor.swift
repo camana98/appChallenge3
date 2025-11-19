9cdbbbbe51507b4c921939d8bf3ea623a91d4231
Pablo Garcia <pablorobertodev@gmail.com>
2025-11-19 16:36:52 -0300

    Created ViewModelAuthor file

--- appChallenge3/ViewModels/ViewModelAuthor.swift
+++ appChallenge3/ViewModels/ViewModelAuthor.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+import SwiftData
+
+@MainActor
+final class ViewModelAuthor: ObservableObject {
+    var objectWillChange: ObservableObjectPublisher
+    
+    
+    @Published var authors: [Author] = []
+    
+    private var context: ModelContext
+    
+    init(context: ModelContext) {
+        self.objectWillChange = .init()
+        self.context = context
+//        fetchAuthors()
+    }
+    
+    func createAuthor() {
+        
+    }
+    
+    func fetchAuthors() {
+        
+    }
+    
+}
