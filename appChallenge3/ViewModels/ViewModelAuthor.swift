fa5c86364b49161d4be8d0dc4a5559c3356c1246
Pablo Garcia <pablorobertodev@gmail.com>
2025-11-19 17:27:58 -0300

    CRD Author has the based functions done.

--- appChallenge3/ViewModels/ViewModelAuthor.swift
+++ appChallenge3/ViewModels/ViewModelAuthor.swift
@@ -22,15 +22,46 @@ final class ViewModelAuthor: ObservableObject {
     init(context: ModelContext) {
         self.objectWillChange = .init()
         self.context = context
-//        fetchAuthors()
+        fetchAuthors()
     }
     
-    func createAuthor() {
+    //MARK: - Create Authors
+    
+    func createAuthor(name: String, appleUserID: String) {
+        let newAuthor = Author(name: name, appleUserID: appleUserID)
+        context.insert(newAuthor)
         
+        
+        do {
+            try context.save()
+            fetchAuthors()
+            
+        } catch {
+            print("Erro ao criar novo autor: \(error)")
+        }
     }
     
+    //MARK: - Read Authors
     func fetchAuthors() {
-        
+        let descriptor = FetchDescriptor<Author>(
+            sortBy: [SortDescriptor(\.createdAt, order: .reverse)]
+        )
+
+        do {
+            authors = try context.fetch(descriptor)
+        } catch {
+            print("Erro ao buscar autores: \(error)")
+        }
     }
     
+    func deleteAuthor(_ author: Author) {
+        context.delete(author)
+        
+        do {
+            try context.save()
+            fetchAuthors()
+        } catch {
+            print("Erro ao deletar autor: \(error)")
+        }
+    }
 }
