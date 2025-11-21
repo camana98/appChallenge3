da167790d69cca86267fbea7391d605cb5eb71f3
Pablo Garcia <pablorobertodev@gmail.com>
2025-11-21 17:17:50 -0300

    Correçao dos comentários dos arquivos

--- appChallenge3Tests/MemoryContainer.swift
+++ appChallenge3Tests/MemoryContainer.swift
@@ -6,23 +6,6 @@
 //
 //  Criado para testes in-memory usando SwiftData
 //
-//import Foundation
-//import SwiftData
-//
-//enum MemoryContainer {
-//
-//    /// Cria um ModelContainer em memória para testes
-//    static func makeInMemoryContainer() throws -> ModelContainer {
-//        let config = ModelConfiguration(isStoredInMemoryOnly: true)
-//
-//        let container = try ModelContainer(
-//            for: Author.self,
-//            configurations: config
-//        )
-//
-//        return container
-//    }
-//}
 
 import SwiftData
 @testable import appChallenge3
