0dd7f846fe4cc057651b34ef231269daa6c5c7a7
Guilherme Ghise <guilhermeghise7@gmail.com>
2025-11-21 13:38:17 -0300

    fix main

---
+++ Glyptis/App/GlyptisApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlyptisApp.swift
+//  Glyptis
+//
+//  Created by Vicenzo Másera on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GlyptisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
