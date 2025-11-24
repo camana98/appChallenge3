5a1a249c88807b750316c55b5df68fa3e8cb3e18
vicenzorm <vimasera11@gmail.com>
2025-11-24 14:22:45 -0300

    defined coordinator

---
+++ Glyptis/Views/ARCameraView/ARCameraView.swift
@@ -0,0 +1,18 @@
+//
+//  ARCameraView.swift
+//  Glyptis
+//
+//  Created by Vicenzo Másera on 24/11/25.
+//
+
+import SwiftUI
+
+struct ARCameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARCameraView()
+}
---
+++ Glyptis/Views/ARCameraView/ARViewContainer.swift
@@ -0,0 +1,8 @@
+//
+//  ARViewContainer.swift
+//  Glyptis
+//
+//  Created by Vicenzo Másera on 24/11/25.
+//
+
+import Foundation
