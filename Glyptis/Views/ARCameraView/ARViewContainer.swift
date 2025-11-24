637b11e6ca154aa6862d25334f1e819ea9ba0d0c
vicenzorm <vimasera11@gmail.com>
2025-11-24 14:25:21 -0300

    finished aux struct that helps to translate to swiftui

--- Glyptis/Views/ARCameraView/ARViewContainer.swift
+++ Glyptis/Views/ARCameraView/ARViewContainer.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import ARKit
+import RealityKit
+import SwiftUI
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    @Binding var coordinator: ARViewCoordinator
+    
+    func makeUIView(context: Context) -> some UIView {
+        // chama so quando é instanciada pela primeira vez
+        return coordinator.setupARView()
+        
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        // não faz nada
+    }
+    
+}
