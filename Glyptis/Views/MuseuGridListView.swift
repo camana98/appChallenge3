e5a565df83f464a01ecd43bbac1f57209808b5e2
vicenzorm <vimasera11@gmail.com>
2025-11-26 14:34:06 -0300

    preparing for other users entering the branch

---
+++ Glyptis/Views/MuseuGridListView.swift
@@ -0,0 +1,18 @@
+//
+//  MuseuGridListView.swift
+//  Glyptis
+//
+//  Created by Vicenzo Másera on 26/11/25.
+//
+
+import SwiftUI
+
+struct MuseuGridListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MuseuGridListView()
+}
