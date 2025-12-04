ed096a80eca3a0509c8cf5bcceacb2f921736a03
Guilherme Ghise <guilhermeghise7@gmail.com>
2025-12-04 17:24:03 -0300

    empty state 70% finalizado

---
+++ Glyptis/Features/Museu/MuseuEmptyStateView.swift
@@ -0,0 +1,63 @@
+//
+//  MuseuView.swift
+//  Glyptis
+//
+//  Created by Vicenzo Másera on 25/11/25.
+//
+
+import Foundation
+import SwiftUI
+internal import RealityKit
+import SwiftData
+
+struct MuseuView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var showGridListMuseum: Bool = false
+    var onBackClicked: () -> Void
+    
+    var body: some View {
+        ZStack {
+            
+            Image(.backgroundMuseu)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    
+                    SimpleCubeIcon(assetName: "backCube", width: 55, height: 55) {
+                        onBackClicked()
+                    }
+                    
+                    Spacer()
+                    
+                    Text("Museu")
+                        .font(.custom("Angle Square DEMO", size: 24))
+                        .foregroundStyle(.customWhite)
+                    
+                    Spacer()
+                    
+                    SimpleCubeIcon(assetName: "gridCube", width: 55, height: 55) {
+                        showGridListMuseum.toggle()
+                    }
+                }
+                .padding(.top)
+                
+                Spacer()
+                                
+            }
+        }
+        .sheet(isPresented: $showGridListMuseum) {
+            MuseuGridListView(vm: MuseuGridViewModel(context: modelContext, service: SculptureService(context: modelContext)))
+                .presentationDetents([.medium, .large])
+                .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+        }
+    }
+}
+
+#Preview {
+    MuseuView(onBackClicked: {})
+}
